4568c50eeaff8ac4c0de2210a2768b8aada1c3f1
Cage <517020375@qq.com>
2011-11-01 17:38:14 +0800

    ...

--- static/js/base.js
+++ static/js/base.js
@@ -1,17 +1,22 @@
 $(document).ready(function(){
 	var $content = $("#content");
 	$("#head,#foot").css("width",$content.outerWidth());
+    $(window).resize(function(){
+        $("#head,#foot").css("width",$content.outerWidth());
+    })
 	setTimeout(function(){
 		$("#loading").css("display","none");
 	},1000);
-	if($.browser.msie){
-		if($.browser.version == '6.0'){
-			var $img = $("#details img");
-			if($img.width() > 840){
-				$img.css('width','840px')
-			}
-		}
-	}
+    /*
+	 *if($.browser.msie){
+	 *    if($.browser.version == '6.0'){
+	 *        var $img = $("#details img");
+	 *        if($img.width() > 840){
+	 *            $img.css('width','840px')
+	 *        }
+	 *    }
+	 *}
+     */
 	$("#replay_form").submit(function(){
 		if($("#id_name").val().length ==0 || $("#id_message").val().length == 0){
 			alert("名称和内容是必填项!")
